fcf34da5ccbdb9fd20a0da557892245714b74ccc
sabzyhacks <ei711283@gmail.com>
2026-07-30 21:53:16 +0100

    initial question display added

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const questionScreen = document.querySelector("#questionScreen");
 const resultsScreen = document.querySelector("#resultsScreen");
 
 const startBtn = document.querySelector("#startBtn");
+
+startBtn.addEventListener("click", function () {
+  startScreen.classList.add("hidden");
+  questionScreen.classList.remove("hidden");
+});
