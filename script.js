7ceb1dbeb2feb44e1aadc55da39264dfe6f2e583
sabzyhacks <ei711283@gmail.com>
2026-08-01 17:21:15 +0100

    testing...

--- script.js
+++ script.js
@@ -187,6 +187,8 @@ optionsContainer.addEventListener("click", function (e) {
       currentQuestion++;
       setTimeout(() => {
         isClicked = false;
+        e.target.style.background = "rgba(255, 255, 255, 0.07)";
+        e.target.style.border = "0.5px solid rgba(255, 255, 255, 0.15)";
         dynamicHtml();
       }, 2000);
     }
